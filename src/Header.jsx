aa5a2767e01f2e801476caa3d4d49f2607a8b86e
andrejlizak <andrej.lizak@student.tuke.sk>
2023-11-16 21:49:43 +0100

    header with no funtion in btns

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,23 @@
 import './Header.scss';
+
 function Header(){
 
     return(
         <header>
-            <h1>Andrej Lizák</h1>
+            <h1>Andrej <br></br>Lizák</h1>
+            <p>I am an ambitious I. degree software engineer student of <br></br>
+            Technical University of Košice, who <br></br>
+            loves to build new projects and learn on daily basis. <br></br>
+            I am looking forward to new coding challenges of <strong>all kinds!</strong>
+            </p>
+            <button class="btn download">
+                DOWNLOAD CV
+            </button>
+
+            <button class ="btn discover">
+                DISCOVER MORE
+            </button>
+
         </header>
     );
 }
